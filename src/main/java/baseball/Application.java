b3f6ddb8839f44ac89441a89ead7a9a33048c508
tseop <77163842+tseop@users.noreply.github.com>
2022-11-10 13:56:39 +0900

    Feat(baseball): 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computers = generator.createRandomNumbers();
+        System.out.println("computers = " + computers);
     }
 }
