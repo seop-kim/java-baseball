70a61c294a670766de79dd6243392871c6e8778e
tseop <77163842+tseop@users.noreply.github.com>
2022-12-06 16:23:57 +0900

    refactor: Separation of concerns

--- src/main/java/baseball/domain/Comparator.java
+++ src/main/java/baseball/domain/Comparator.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class Comparator {
 
-    private static final String RETRY = "1";
-    private static final String NO_RETRY = "2";
-    private static final String RETRY_EXCEPTION_MSG = "1과 2의 값만 입력이 가능합니다.";
-
     public int ballCompare(List<Integer> playerNumbers, List<Integer> computerNumbers) {
         int result = 0;
         for (int number : playerNumbers) {
@@ -36,15 +32,5 @@ public class Comparator {
             return 1;
         }
         return 0;
-    }
-
-    public boolean retry(String input) {
-        if (input.equals(RETRY)) {
-            return true;
-        }
-        if (input.equals(NO_RETRY)) {
-            return false;
-        }
-        throw new IllegalArgumentException(RETRY_EXCEPTION_MSG);
     }
 }
