2313f2e02cd651b8087ce3a325b261a2f3e859a2
tseop <77163842+tseop@users.noreply.github.com>
2022-12-06 17:01:53 +0900

    refactor: Refactoring code
    Refactored to less than 10 lines
    message constant
    function name change

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,37 +1,58 @@
 package baseball.view;
 
 public class OutputView {
-    public void printGameStartMsg() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private static final String START_MSG = "숫자 야구 게임을 시작합니다.";
+    private static final String REQUEST_INPUT_MSH = "숫자를 입력해주세요 : ";
+    private static final String SCORE_NO_MATCH = "낫싱";
+    private static final String SCORE_ALL_MATCH = "%d볼 %d스트라이크\n";
+    private static final String SCORE_BALL_MATCH = "%d볼\n";
+    private static final String SCORE_STRIKE_MATCH = "%d스트라이크\n";
+    private static final String GAME_FINISH_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String REQUEST_RETRY_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printScore(int strikeScore, int ballScore) {
+        finishGame(strikeScore, ballScore);
+        zeroScore(strikeScore, ballScore);
+        oneScoreMatch(strikeScore, ballScore);
+        allScore(strikeScore, ballScore);
     }
 
-    public void printInputRequestMsg() {
-        System.out.print("숫자를 입력해주세요 : ");
+    public void printGameStart() {
+        System.out.println(START_MSG);
     }
 
-    public void printScoreMsg(int strikeScore, int ballScore) {
-        if (strikeScore == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            return;
-        }
+    public void printInputRequest() {
+        System.out.print(REQUEST_INPUT_MSH);
+    }
 
+    private void zeroScore(int strikeScore, int ballScore) {
         if (strikeScore == 0 && ballScore == 0) {
-            System.out.println("낫싱");
-            return;
+            System.out.println(SCORE_NO_MATCH);
         }
+    }
 
+    private void allScore(int strikeScore, int ballScore) {
+        if (strikeScore != 0 && ballScore != 0 && strikeScore <= 3 && ballScore <= 3) {
+            System.out.printf(SCORE_ALL_MATCH, ballScore, strikeScore);
+        }
+    }
+
+    private void finishGame(int strikeScore, int ballScore) {
+        if (strikeScore == 3 && ballScore == 0) {
+            System.out.printf(SCORE_STRIKE_MATCH, strikeScore);
+            System.out.println(GAME_FINISH_MSG);
+            System.out.println(REQUEST_RETRY_MSG);
+        }
+    }
+
+    private void oneScoreMatch(int strikeScore, int ballScore) {
         if (strikeScore == 0 && ballScore <= 3) {
-            System.out.println(ballScore + "볼");
-            return;
+            System.out.printf(SCORE_BALL_MATCH, ballScore);
         }
 
         if (ballScore == 0 && strikeScore <= 3) {
-            System.out.println(strikeScore + "스트라이크");
-            return;
+            System.out.printf(SCORE_STRIKE_MATCH, strikeScore);
         }
-
-        System.out.println(ballScore + "볼 " + strikeScore + "스트라이크");
     }
+
 }
