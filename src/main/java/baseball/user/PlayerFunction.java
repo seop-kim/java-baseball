a54f6fa3b44e26000735aa13563352d2200ee7fd
tseop <77163842+tseop@users.noreply.github.com>
2022-12-06 10:11:21 +0900

    rename: UserFunction rename
    UserFunction -> PlayerFunction

--- src/main/java/baseball/user/PlayerFunction.java
+++ src/main/java/baseball/user/PlayerFunction.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class UserFunction {
+public class PlayerFunction {
 
     public List<Integer> createUserList(String input) {
         if (!inputLengthCheck(input)) {
