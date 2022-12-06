adb587c8484b3c039a6c5f823dddd0cc3bf41222
tseop <77163842+tseop@users.noreply.github.com>
2022-12-06 18:06:29 +0900

    refactor: Edit variable name

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 public class Game {
     private static final PlayerConvertor PLAYER = new PlayerConvertor();
-    private static final PlayerValidator VALIDATOR = new PlayerValidator();
     private static final NumbersGenerator COMPUTER = new NumbersGenerator();
-    private static final Comparator COM = new Comparator();
-    private static final OutputView OUTPUT_VIEW = new OutputView();
-    private static List<Integer> computerNumList;
-    private static List<Integer> playerNumbers;
+    private static final PlayerValidator VALIDATOR = new PlayerValidator();
+    private static final Comparator COMPARATOR = new Comparator();
     private static final InputView INPUT_VIEW = new InputView();
+    private static final OutputView OUTPUT_VIEW = new OutputView();
+    private static List<Integer> COMPUTER_NUMBERS;
+    private static List<Integer> PLAYER_NUMBERS;
 
     private static final String RETRY = "1";
     boolean playingGames = false;
@@ -26,43 +26,41 @@ public class Game {
     boolean gameAgain = true;
 
     public void go() {
-        OUTPUT_VIEW.printGameStartMsg();
         while (gameAgain) {
             isPlayingGames();
-            playerNumbers = getPlayerNumbers();
+            PLAYER_NUMBERS = requestPlayerNumbers();
             calScore();
-            OUTPUT_VIEW.printScoreMsg(strikeScore, ballScore);
+            OUTPUT_VIEW.printScore(strikeScore, ballScore);
             isGameEnd();
         }
     }
 
     private void calScore() {
-        strikeScore = COM.strikeCompare(playerNumbers, computerNumList);
-        ballScore = COM.ballCompare(playerNumbers, computerNumList) - strikeScore;
+        strikeScore = COMPARATOR.strikeCompare(PLAYER_NUMBERS, COMPUTER_NUMBERS);
+        ballScore = COMPARATOR.ballCompare(PLAYER_NUMBERS, COMPUTER_NUMBERS) - strikeScore;
     }
 
     private void isPlayingGames() {
         if (!playingGames) {
-            computerNumList = COMPUTER.createNumbers();
+            COMPUTER_NUMBERS = COMPUTER.createNumbers();
             playingGames = true;
         }
     }
 
     private void isGameEnd() {
         if (strikeScore == 3) {
-            userInput = INPUT_VIEW.requestPlayerInput();
-            gameAgain = retry(userInput);
+            gameAgain = retry(INPUT_VIEW.requestPlayerInput());
             playingGames = false;
         }
     }
 
-    private List<Integer> getPlayerNumbers() {
-        OUTPUT_VIEW.printInputRequestMsg();
+    private List<Integer> requestPlayerNumbers() {
+        OUTPUT_VIEW.printInputRequest();
         return PLAYER.createPlayerNumbers(INPUT_VIEW.requestPlayerInput());
     }
 
     private boolean retry(String input) {
-        VALIDATOR.retryValidation(input); // Validator 에서 이미 1 과 2의 검증은 끝난다.
+        VALIDATOR.retryValidation(input); // Validator 에서 1 과 2의 검증은 끝난다.
         return input.equals(RETRY);
     }
 }
